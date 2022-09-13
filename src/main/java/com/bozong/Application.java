ab5948aeb7f089065bc51ce5bbe09a9ca7b6e955
yinguoyu <yinguoyu@abupdate.com>
2022-09-13 18:18:53 +0800

    init

---
+++ src/main/java/com/bozong/Application.java
@@ -0,0 +1,16 @@
+package com.bozong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinguoyu
+ * @date 2022/9/13
+ * @des
+**/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
